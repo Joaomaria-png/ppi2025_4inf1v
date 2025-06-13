face7de6aa88deeb760ce9430d3cde38261c6cb1
Joaomaria-png <neto.maria@escolar.ifrn.edu.br>
2025-06-13 23:09:40 +0000

    Atividade Avaliativa - ETAPA 1

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,8 +1,19 @@
 import styles from "../components/MyFooter.module.css";
+import { Facebook } from "lucide-react";
+import { Instagram } from "lucide-react";
+import { Phone } from "lucide-react";
 export function Myfooter() {
   return (
       <footer className={styles.footer}>
-        <p>&copy; 2025 Minha Loja. Todos os direitos reservados.</p>
+        <p className={styles.text}>IFRN - Campus Macau
+        <br/>Curso Tecnico em informatica
+        <br/>Programação para a internet 2025</p>
+        <p className = {styles.assinatura}>Joao maria de lima pereira neto</p>
+        <div className={styles.icons}>
+          <Facebook />
+          <Instagram />
+          <Phone />
+        </div>
       </footer>
   );
 }
