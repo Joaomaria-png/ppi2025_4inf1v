ef5b80c69d36b5d71aebf8c586618f50c66734f0
Joaomaria-png <neto.maria@escolar.ifrn.edu.br>
2025-06-25 17:28:49 +0000

    INICIO DE DESAFIO

--- src/components/E2/LuckyNumber.jsx
+++ src/components/E2/LuckyNumber.jsx
@@ -1,19 +1,51 @@
 import { useState } from 'react';
 import styles from './LuckyNumber.module.css';
 
-export  function LuckyNumber() {
-  const [luckyNumber, setLuckyNumber] = useState(0);
+export function LuckyNumber() {
+    // React HOOK - useState()
+    const [luckyNumber, setLuckyNumber] = useState(0);
+    function handleLuckyNumber() {
+        setLuckyNumber(Math.ceil(Math.random() * 31));
+        // O valor atualizado só estará disponível no próximo render
+        console.log(`LuckyNumber atualizado: valor será atualizado`);
+        let history = [];
+        history.push(luckyNumber);
+        console.log('Histórico de Lucky Numbers:', history);
+    }
+    function handleIncrement() {
+        setLuckyNumber((prev) => prev + 1);
+        console.log(`LuckyNumber atualizado: valor será atualizado`);
+    }
+    function handleDecrement() {
+        setLuckyNumber((prev) => prev - 1);
+        console.log(`LuckyNumber atualizado: valor será atualizado`);
+    }
+    function handleZero() {
+        setLuckyNumber(0);
+        console.log(`LuckyNumber atualizado: 0`);
+    }
 
-  function handleIncrement() {
-    setLuckyNumber((prev) => prev + 1);
-  }
+    return (
+        <div className={styles.container}>
+            {LuckyNumber ? (
+                <h1>Lucky Number = {luckyNumber}</h1>
+            ):
+            (
+                <h1>Lucky Number 🎲</h1>
+            )}
 
-  return (
-    <div className={styles.container}>
-      <h1>Contador = {luckyNumber}</h1>
-      <button className={styles.button} onClick={handleIncrement}>
-        Incrementar em 1
-      </button>
-    </div>
-  );
+            <button className={styles.button} onClick={handleLuckyNumber}>
+                Random
+            </button>
+            <button className={styles.button} onClick={handleIncrement}>
+                + 1
+            </button>
+            <button className={styles.button} onClick={handleDecrement}>
+                - 1
+            </button>
+            <button className={styles.button} onClick={handleZero}>
+                = 0
+            </button>
+        </div>
+    );
 }
